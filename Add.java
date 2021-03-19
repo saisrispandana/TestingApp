9c71f69e3d3d4b0aa677e911316404a82d7f67c8
saisrispandana <79159088+saisrispandana@users.noreply.github.com>
2021-03-19 13:57:34 +0530

    Add files via upload
    commitsamp1

---
+++ Add.java
@@ -0,0 +1,9 @@
+public class Add {
+  
+     public static void main(String[] args) {
+          // TODO Auto-generated method stub
+          int a=3,b=2,c;
+          c=a+b;
+          System.out.println("C value"+c);
+          }
+}
